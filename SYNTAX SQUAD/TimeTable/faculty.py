771249ad9c8a17bb144acff18351e14cfd96a9c3
Reeteshk937 <reeteshk937@gmail.com>
2025-10-05 23:40:24 +0530

    Delete SYNTAX SQUAD directory

--- SYNTAX SQUAD/TimeTable/faculty.py
+++
@@ -1,9 +0,0 @@
-def create_faculty(name, department, availability):
-    """
-    Create a faculty dictionary.
-    """
-    return {
-        "name": name,
-        "department": department,
-        "availability": availability
-    }
--- SYNTAX SQUAD/TimeTable/room.py
+++
@@ -1,9 +0,0 @@
-def create_room(room_number, capacity, type_="Lecture"):
-    """
-    Create a room dictionary.
-    """
-    return {
-        "room_number": room_number,
-        "capacity": capacity,
-        "type": type_
-    }
